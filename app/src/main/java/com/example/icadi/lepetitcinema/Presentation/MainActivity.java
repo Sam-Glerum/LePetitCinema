7301c346972d56e7eae22cf60b780344a7ce9c20
Bas Cijsouw <hmscijso@avans.nl>
2018-03-28 15:42:07 +0200

    Strings aangepast aan contactscherm, contactscherm in de XML-file gemaakt. Nog niet gelinkt aan de menubar

--- app/src/main/java/com/example/icadi/lepetitcinema/Presentation/MainActivity.java
+++ app/src/main/java/com/example/icadi/lepetitcinema/Presentation/MainActivity.java
@@ -1,18 +1,17 @@
 package com.example.icadi.lepetitcinema.Presentation;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toolbar;
 
 import com.example.icadi.lepetitcinema.Domain.Film;
 import com.example.icadi.lepetitcinema.R;
