4240ff2e0018200b4b41c36b31e3f87a89239e6f
Niek Flipse <npaflips@avans.nl>
2018-03-28 14:43:19 +0200

    SuccesPaymentActivity now has view components and one fuctionality

--- app/src/main/java/com/example/icadi/lepetitcinema/Presentation/SuccessPaymentActivity.java
+++ app/src/main/java/com/example/icadi/lepetitcinema/Presentation/SuccessPaymentActivity.java
@@ -1,13 +1,29 @@
 package com.example.icadi.lepetitcinema.Presentation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class SuccessPaymentActivity extends AppCompatActivity {
+import com.example.icadi.lepetitcinema.R;
+
+public class SuccessPaymentActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button backToHomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_success_payment2);
+        setContentView(R.layout.activity_success_payment);
+
+        backToHomeButton = findViewById(R.id.backToHomeButton);
+        backToHomeButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent toHomeScreen = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(toHomeScreen);
     }
 }
