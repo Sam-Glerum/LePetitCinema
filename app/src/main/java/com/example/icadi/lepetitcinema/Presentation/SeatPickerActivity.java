50825a8905efa980e84e84896c2133dac6109dcc
Unknown <arjangosens@hotmail.com>
2018-03-28 16:34:27 +0200

    Added onclicklistener to all imageviews, imageviews now change colour

--- app/src/main/java/com/example/icadi/lepetitcinema/Presentation/SeatPickerActivity.java
+++ app/src/main/java/com/example/icadi/lepetitcinema/Presentation/SeatPickerActivity.java
@@ -3,11 +3,12 @@ package com.example.icadi.lepetitcinema.Presentation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.example.icadi.lepetitcinema.R;
 
-public class SeatPickerActivity extends AppCompatActivity {
+public class SeatPickerActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageView[][] seatImageViews;
 
@@ -15,6 +16,8 @@ public class SeatPickerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seat_picker);
+
+        // Create 2d array for the ImageViews
         seatImageViews = getAllSeats();
 
         // Calculate and log the size of the 2d array
@@ -36,10 +39,28 @@ public class SeatPickerActivity extends AppCompatActivity {
                 String seatId = ("r" + (i + 1) + "s" + (ii + 1));
                 int resId = getResources().getIdentifier(seatId, "id", getPackageName());
                 ImageView imageView = (ImageView) findViewById(resId);
+                imageView.setOnClickListener(this);
                 imageViews[i][ii]= imageView;
             }
         }
-
         return imageViews;
+    }
+
+    @Override
+    public void onClick(View view) {
+        ImageView imageView = findViewById(view.getId());
+        if (imageView.getDrawable().getConstantState().equals(getResources().getDrawable(R.drawable.cinemaseat_available).getConstantState())) {
+            imageView.setImageDrawable(getResources().getDrawable(R.drawable.cinemaseat_selected));
+
+        } else if (imageView.getDrawable().getConstantState().equals(getResources().getDrawable(R.drawable.cinemaseat_selected).getConstantState())) {
+            imageView.setImageDrawable(getResources().getDrawable(R.drawable.cinemaseat_available));
+
+        } else if (imageView.getDrawable().getConstantState().equals(getResources().getDrawable(R.drawable.cinemaseat_occupied).getConstantState())) {
+            Log.i("SeatPickerActivity", "The chosen seat is already occupied!");
+        } else {
+            Log.i("SeatPickerActivity", "ERROR: Could not get drawable to check!");
+        }
+
+
     }
 }
