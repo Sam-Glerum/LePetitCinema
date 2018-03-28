aa77150d7cb019b1d9ffc92e618b0e1b0ffca862
Niek Flipse <npaflips@avans.nl>
2018-03-28 14:41:50 +0200

    Added empty PaymentSimulationActivity and SuccesPaymentActivity

---
+++ app/src/main/java/com/example/icadi/lepetitcinema/Presentation/PaymentSimulationActivity.java
@@ -0,0 +1,13 @@
+package com.example.icadi.lepetitcinema.Presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PaymentSimulationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_simulation);
+    }
+}
