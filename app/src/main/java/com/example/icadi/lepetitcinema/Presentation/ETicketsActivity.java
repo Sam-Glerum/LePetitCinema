92e9213d3c2eaa8ff53bd46248f924380e810cb0
Niek Flipse <npaflips@avans.nl>
2018-03-28 17:31:48 +0200

    Created Intent to the ETicketDetailActivity

--- app/src/main/java/com/example/icadi/lepetitcinema/Presentation/ETicketsActivity.java
+++ app/src/main/java/com/example/icadi/lepetitcinema/Presentation/ETicketsActivity.java
@@ -1,8 +1,11 @@
 package com.example.icadi.lepetitcinema.Presentation;
 
+import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.example.icadi.lepetitcinema.Domain.Ticket;
@@ -10,7 +13,12 @@ import com.example.icadi.lepetitcinema.R;
 
 import java.util.ArrayList;
 
-public class ETicketsActivity extends AppCompatActivity {
+public class ETicketsActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
+
+    public static String E_TICKET_FILM_NAME = "E_TICKET_FILM_NAME";
+    public static String E_TICKET_SEAT_NUMBER = "E_TICKET_SEAT_NUMBER";
+    public static String E_TICKET_CINEMA_ROOM = "E_TICKET_CINEMA_ROOM";
+    public static String E_TICKET_DATE_TIME = "E_TICKET_DATE_TIME";
 
     private DrawerLayout drawerLayout;
     private ListView listView;
@@ -30,5 +38,19 @@ public class ETicketsActivity extends AppCompatActivity {
 
         eTicketsAdapter = new ETicketsAdapter(getApplicationContext(), getLayoutInflater(), tickets);
         listView.setAdapter(eTicketsAdapter);
+
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Intent toDetail = new Intent(getApplicationContext(), ETicketDetailActivity.class);
+
+        toDetail.putExtra(E_TICKET_FILM_NAME, tickets.get(i).getFilmName());
+        toDetail.putExtra(E_TICKET_SEAT_NUMBER, tickets.get(i).getSeatNumber());
+        toDetail.putExtra(E_TICKET_DATE_TIME, tickets.get(i).getDate() + " " + tickets.get(i).getTime());
+        toDetail.putExtra(E_TICKET_CINEMA_ROOM, tickets.get(i).getCinemaRoom());
+
+        startActivity(toDetail);
     }
 }
