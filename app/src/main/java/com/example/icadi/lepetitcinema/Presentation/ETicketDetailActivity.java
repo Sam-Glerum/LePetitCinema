b66f148211bde7ba53750f63aeb7a4ad514a5a45
Niek Flipse <npaflips@avans.nl>
2018-03-28 16:56:34 +0200

    Created empty ETicketDetailActivity

---
+++ app/src/main/java/com/example/icadi/lepetitcinema/Presentation/ETicketDetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.icadi.lepetitcinema.Presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.icadi.lepetitcinema.R;
+
+public class ETicketDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eticket_detail);
+    }
+}
