e49167fb929e8c28816f27328d3f76254a58966b
Niek Flipse <npaflips@avans.nl>
2018-03-28 16:55:47 +0200

    Added a single hardcode ticked

--- app/src/main/java/com/example/icadi/lepetitcinema/Presentation/ETicketsActivity.java
+++ app/src/main/java/com/example/icadi/lepetitcinema/Presentation/ETicketsActivity.java
@@ -24,6 +24,10 @@ public class ETicketsActivity extends AppCompatActivity {
 
         listView = findViewById(R.id.ticketsListView);
 
+        Ticket ticket1 = new Ticket("Jumanji", "1", "3", "", null, "29-3-2018", "16:00");
+
+        tickets.add(ticket1);
+
         eTicketsAdapter = new ETicketsAdapter(getApplicationContext(), getLayoutInflater(), tickets);
         listView.setAdapter(eTicketsAdapter);
     }
