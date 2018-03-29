12b29e237a00ba7e60786e4711873f42caf9431e
Arjan Gosens <arjangosens@hotmail.com>
2018-03-29 16:36:35 +0200

    Created check and alert dialog for inequal value between seats and tickets
    At this point, ordering 0 tickets and seats is still possible, will tackle that next

--- app/src/main/java/com/example/icadi/lepetitcinema/Presentation/SeatPickerActivity.java
+++ app/src/main/java/com/example/icadi/lepetitcinema/Presentation/SeatPickerActivity.java
@@ -1,6 +1,8 @@
 package com.example.icadi.lepetitcinema.Presentation;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -129,13 +131,36 @@ public class SeatPickerActivity extends AppCompatActivity implements View.OnClic
 
         switch (view.getId()) {
             case R.id.seatPicker_button_buyTickets:
-                Intent toPayment = new Intent(getApplicationContext(), PaymentSimulationActivity.class);
-                toPayment.putExtra(SEATS, currentlySelectedSeats);
-                toPayment.putExtra(FILMTITLE, getIntent().getStringExtra(FILMTITLE));
-                toPayment.putExtra(PRICE, totalPrice);
-                toPayment.putExtra(AMOUNTOFTICKETS, "" + currentlySelectedSeats.size());
 
-                startActivity(toPayment);
+                int currentAmountOfSeats = currentlySelectedSeats.size();
+                int currentAmountOfTickets = childAmount + normalAmount + elderAmount;
+
+                if (currentAmountOfSeats == currentAmountOfTickets) {
+                    Intent toPayment = new Intent(getApplicationContext(), PaymentSimulationActivity.class);
+                    toPayment.putExtra(SEATS, currentlySelectedSeats);
+                    toPayment.putExtra(FILMTITLE, getIntent().getStringExtra(FILMTITLE));
+                    toPayment.putExtra(PRICE, totalPrice);
+                    toPayment.putExtra(AMOUNTOFTICKETS, "" + currentlySelectedSeats.size());
+
+                    startActivity(toPayment);
+
+                } else {
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+                    alertDialogBuilder.setTitle("Amount of tickets and seats are not equal!");
+                    alertDialogBuilder.setMessage("There are currently " + currentAmountOfSeats +
+                    " seats and " + currentAmountOfTickets + " tickets selected");
+                    alertDialogBuilder.setCancelable(false);
+                    alertDialogBuilder.setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog,int id) {
+                            dialog.cancel();
+                        }
+                    });
+
+                    AlertDialog alertDialog = alertDialogBuilder.create();
+
+                    alertDialog.show();
+                }
                 break;
 
             case R.id.seatPicker_button_childDecrease:
