a6abe293a801e97de27fd6a2d559189b6634fb45
Niek Flipse <npaflips@avans.nl>
2018-04-04 21:03:05 +0200

    FilmDetailactivity shows reviews

--- app/src/main/java/com/example/icadi/lepetitcinema/Presentation/FilmDetailActivity.java
+++ app/src/main/java/com/example/icadi/lepetitcinema/Presentation/FilmDetailActivity.java
@@ -4,23 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.icadi.lepetitcinema.Domain.Film;
+import com.example.icadi.lepetitcinema.Domain.Review;
 import com.example.icadi.lepetitcinema.R;
 import com.squareup.picasso.Picasso;
 
-//import com.example.icadi.lepetitcinema.ApplicationLogic.ImageManager;
-
-public class FilmDetailActivity extends AppCompatActivity implements View.OnClickListener {
+public class FilmDetailActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Film film;
 
     private TextView filmTitle;
     private TextView filmDescription;
+    private TextView reviews;
 
     private FloatingActionButton buyTicketsButton;
     private FloatingActionButton sendReviewButton;
@@ -32,7 +31,7 @@ public class FilmDetailActivity extends AppCompatActivity implements View.OnClic
         setContentView(R.layout.activity_film_detail);
 
         setObjectVariables();
-        
+
         film = (Film) getIntent().getSerializableExtra(MainActivity.FILM);
 
         setObjectVariablesContent();
@@ -52,6 +51,7 @@ public class FilmDetailActivity extends AppCompatActivity implements View.OnClic
         buyTicketsButton = findViewById(R.id.detail_activity_order_ticket_fab);
         filmBackgroundImage = findViewById(R.id.detail_activity_film_image);
         sendReviewButton = findViewById(R.id.detail_activity_review_fab);
+        reviews = findViewById(R.id.detail_activity_reviews);
     }
 
     /**
@@ -65,13 +65,13 @@ public class FilmDetailActivity extends AppCompatActivity implements View.OnClic
 
         filmTitle.setText(film.getName());
         filmDescription.setText(film.getDescription());
-        if (!film.reviewArrayList.isEmpty()){
-            Log.i("TESTBEFOREREFRESH", film.reviewArrayList.toString());
-            //TODO: Add review to XML list
-            finish();
-            startActivity(getIntent());
-            Log.i("TESTAFTERREFRESH", film.reviewArrayList.toString());
+
+        String allReviewsInString = "";
+        for (Review review : film.getReviewArrayList()) {
+            allReviewsInString += review;
         }
+
+        reviews.setText(allReviewsInString);
     }
 
     @Override
