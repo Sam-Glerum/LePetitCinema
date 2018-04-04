4f5e4a6277ca205b92fcefa8eb9381b16a48a995
Niek Flipse <npaflips@avans.nl>
2018-04-04 21:05:18 +0200

    deleted unnecessary object variables

--- app/src/main/java/com/example/icadi/lepetitcinema/Presentation/ReviewActivity.java
+++ app/src/main/java/com/example/icadi/lepetitcinema/Presentation/ReviewActivity.java
@@ -1,5 +1,6 @@
 package com.example.icadi.lepetitcinema.Presentation;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
@@ -13,21 +14,12 @@ import com.example.icadi.lepetitcinema.R;
 
 public class ReviewActivity extends AppCompatActivity implements View.OnClickListener {
 
-
-    private TextView headerTextView;
     private TextView titleTextView;
-    private String filmtitle;
 
-    private TextView emailTextview;
     private TextInputEditText emailEnterTextView;
-
-    private TextView enterReviewHeaderTextview;
     private TextInputEditText enterReviewTextView;
 
-    private Button sendReview;
-
     private Film film;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +27,6 @@ public class ReviewActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_review);
         setObjectVariables();
         setObjectVariablesContent();
-
     }
 
     /**
@@ -43,41 +34,37 @@ public class ReviewActivity extends AppCompatActivity implements View.OnClickLis
      */
     private void setObjectVariables() {
         film = (Film) getIntent().getSerializableExtra(MainActivity.FILM);
-        filmtitle = film.getName();
-        headerTextView = findViewById(R.id.reviewact_title);
+
         titleTextView = findViewById(R.id.filmtitle);
-        emailTextview = findViewById(R.id.email);
 
         emailEnterTextView = findViewById(R.id.emailField);
         enterReviewTextView = findViewById(R.id.reviewfield);
-        sendReview = findViewById(R.id.sendreviewbutton);
+        Button sendReview = findViewById(R.id.sendreviewbutton);
 
         sendReview.setOnClickListener(this);
-
     }
 
     /**
      * This method sets the content of the object variables.
      */
     private void setObjectVariablesContent() {
-        titleTextView.setText(filmtitle);
+        titleTextView.setText(film.getName());
     }
 
     @Override
     public void onClick(View v) {
         composeReview();
 
-        finish();
+        Intent toDetail = new Intent(getApplicationContext(), FilmDetailActivity.class);
+        toDetail.putExtra(MainActivity.FILM, film);
+        startActivity(toDetail);
     }
 
     private void composeReview() {
-
         String emailToDisplay = emailEnterTextView.getText().toString();
         String reviewToDisplay = enterReviewTextView.getText().toString();
         Review newReview = new Review(emailToDisplay, reviewToDisplay);
         film.reviewArrayList.add(newReview);
-        //TODO: Add review to film.reviews and connection to FilmDetailActivity
     }
+
 }
-
-
